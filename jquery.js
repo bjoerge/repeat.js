db926964f5ba36a8be29c41ce229166fb3b623f3
Bjørge Næss <bjoerge@bengler.no>
2012-09-03 15:59:08 +0200

    Fix undefined global error

--- jquery.js
+++ jquery.js
@@ -1,5 +1,5 @@
 var Repeat = require('./lib/repeat');
-var jQuery = global.jQuery || require("jquery");
+var jQuery = require("jquery");
  if (parseFloat(jQuery.fn.VERSION) < 1.5) {
    throw new Error("Repeat.js requires jQuery 1.5 or later");
  }
